e8f9300ddc4d388e96d8d62da11dfc8ecc8eef81
dyslo <auau3@kakao.com>
2022-11-20 16:02:47 +0900

    feat: modify form validation function

--- lib/validate.php
+++ lib/validate.php
@@ -3,8 +3,8 @@
     if(ctype_digit($id) == false || $id == null || strlen($id) != 8) return false;
     if(gettype($pw) != "string" || $pw == null || strlen($pw) < 6) return false;
     if(gettype($name) != "string" || $name == null) return false;
-    if(gettype($gender) != "string" || $gender == null) return false;
-    if(ctype_digit($id) == false || $age == null) return false;
+    if(gettype($gender) != "string" || $gender == null || ($gender != "남자" && $gender != "여자")) return false;
+    if(ctype_digit($age) == false || $age == null || strlen($age) > 3) return false;
     return true;
   }
 ?>
