d7c04f7eca0c029d2c1f2e449e6994c9ba148aa9
dyslo <auau3@kakao.com>
2022-11-21 14:43:04 +0900

    feat: modify mysql connection

--- lib/mysql.php
+++ lib/mysql.php
@@ -4,5 +4,5 @@
   $pw     = "password";
   $dbname = "dbname";
 
-  $conn   = mysqli_connect($host, $user, $pw, $dbname);
+  $conn   = mysqli_connect($host, $user, $pw, $dbname) or die('Unabled to connect to Database. Check Connection Configurations');
 ?>
